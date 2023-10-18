61d62b84132a10dae2ee08038873973456ab50a0
haritha <harithaveerappagari@gmail.com>
2023-10-18 08:24:11 +0000

    area of square

---
+++ areas/square.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    float side,area;
+    printf("enter the side length of square :");
+    scanf("%f",&side);
+    area= side*side;
+    printf("area of square =%.2f\n",area);
+    return 0;
+}
+
+
